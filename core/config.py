0ca13010866db459eff9395374b3e3ea439fb7c7
Muhammad-Ihdal <muhammadihdal300@gmail.com>
2026-01-05 15:59:45 +0700

    nulis core/config.py

--- core/config.py
+++ core/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRED_IN_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRED_IN_MINUTES")
